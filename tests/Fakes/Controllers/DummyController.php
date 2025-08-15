0bb8d7dddb700544f89ef69e1b73fc163b469be3
Golluméo <p.mauriello54@gmail.com>
2025-08-16 01:29:44 +0200

    test(fakes): add DummyController for testing purposes
    Introduced a `DummyController` class under `tests/Fakes/Controllers` to serve as a mock controller during test cases. This is intended for verifying specific behaviors and scenarios without relying on actual application controllers.

---
+++ tests/Fakes/Controllers/DummyController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gollumeo\Aegis\Tests\Fakes\Controllers;
+
+final class DummyController
+{
+    public function pay(): void
+    {
+        //
+    }
+}
