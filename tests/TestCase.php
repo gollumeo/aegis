2bfe15358a01cbc5ea113cdf437d64985629d90c
Golluméo <p.mauriello54@gmail.com>
2025-08-16 01:31:45 +0200

    test(tests): enhance test setup with idempotency configurations and routes
    Configured the test environment to include idempotency-related settings and defined a POST route using `DummyController` with the `aegis` middleware. These changes aid in testing idempotency behaviors more effectively.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Gollumeo\Aegis\Tests;
 
 use Gollumeo\Aegis\AegisServiceProvider;
+use Gollumeo\Aegis\Tests\Fakes\Controllers\DummyController;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
@@ -21,6 +22,13 @@ abstract class TestCase extends Orchestra
 
     protected function defineEnvironment($app): void
     {
-        //
+        config(['aegis.require_header' => true]);
+        config(['aegis.header_name' => 'Idempotency-Key']);
+        config(['aegis.methods' => ['POST', 'PUT', 'PATCH', 'DELETE']]);
+    }
+
+    protected function defineRoutes(mixed $router): void
+    {
+        $router->post('/payments', [DummyController::class, 'pay'])->middleware('aegis');
     }
 }
