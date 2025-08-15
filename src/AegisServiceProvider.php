9fe38e815ade2450ca915ca0778e3c21492c4501
Golluméo <p.mauriello54@gmail.com>
2025-08-16 01:22:54 +0200

    feat(service-provider): add middleware alias and configuration publishing
    Registered the `aegis` middleware alias for idempotency handling and added support for publishing `aegis.php` configuration file. These changes streamline middleware usage and provide customization options for package users.

--- src/AegisServiceProvider.php
+++ src/AegisServiceProvider.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Gollumeo\Aegis;
 
+use Gollumeo\Aegis\Infrastructure\Http\Middleware\Idempotency;
 use Illuminate\Support\ServiceProvider;
 
 final class AegisServiceProvider extends ServiceProvider
 {
-    //
+    public function boot(): void
+    {
+        app('router')->aliasMiddleware('aegis', Idempotency::class);
+        $this->publishes([__DIR__.'/../config/aegis.php' => config_path('aegis.php')], 'aegis-config');
+    }
 }
