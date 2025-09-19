6d276ca852bfce572b7ebb6b69115c7c6e85992b
Golluméo <p.mauriello54@gmail.com>
2025-09-20 01:44:40 +0200

    test(unit): add tests for Idempotency-Key length validation
    Expanded `IdempotencyPolicyTest` to include tests verifying `Idempotency-Key` length constraints. Added cases for missing, excessively long, and valid lengths to ensure comprehensive validation. Improved coverage of scenarios related to header length compliance.

--- tests/Unit/IdempotencyPolicyTest.php
+++ tests/Unit/IdempotencyPolicyTest.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use Gollumeo\Aegis\Domain\Exceptions\InvalidIdempotencyKeyLength;
 use Gollumeo\Aegis\Domain\Exceptions\MissingIdempotencyHeader;
 use Gollumeo\Aegis\Domain\Policies\EnsureIdempotencyHeaders;
+use Gollumeo\Aegis\Domain\Policies\EnsureIdempotencyKeyLength;
 use Illuminate\Http\Request;
+use Random\RandomException;
 
 describe('Unit: Idempotency Policy', function (): void {
     it('fails if the Idempotency-Key header is missing', function (): void {
@@ -50,12 +53,57 @@ describe('Unit: Idempotency Policy', function (): void {
 
         $request = Request::create('/payments', 'POST');
 
+        /** @var string $aegisHeaderName */
         $aegisHeaderName = config('aegis.header_name');
 
         $request->headers->set($aegisHeaderName, '123');
 
         expect(
+            /**
+             * @throws InvalidIdempotencyKeyLength
+             */
             fn () => $insurance->assert($request)
         )->toThrow(InvalidIdempotencyKeyLength::class);
+    });
+
+    it('fails if the Idempotency-Key length is too long',
+        /**
+         * @throws RandomException|InvalidIdempotencyKeyLength
+         */
+        function (): void {
+            $insurance = new EnsureIdempotencyKeyLength();
+
+            $request = Request::create('/payments', 'POST');
+
+            /** @var string $aegisHeaderName */
+            $aegisHeaderName = config('aegis.header_name');
+
+            /** @var int<1, max> $maxKeyLength */
+            $maxKeyLength = config('aegis.key.max');
+
+            $randomLongKey = bin2hex(random_bytes(($maxKeyLength))); // `bin2hex` doubles the string anyway (1 byte === 2 hex chars)
+
+            $request->headers->set($aegisHeaderName, $randomLongKey);
+
+            expect(
+                fn () => $insurance->assert($request)
+            )->toThrow(InvalidIdempotencyKeyLength::class);
+        });
+
+    it('succeeds if the Idempotency-Key length is correct', function (): void {
+        $insurance = new EnsureIdempotencyKeyLength();
+
+        $request = Request::create('/payments', 'POST');
+        /** @var string $aegisHeaderName */
+        $aegisHeaderName = config('aegis.header_name');
+
+        $request->headers->set($aegisHeaderName, '123456-789-azerzerzd');
+
+        expect(
+            /**
+             * @throws InvalidIdempotencyKeyLength
+             */
+            fn () => $insurance->assert($request)
+        )->not->toThrow(InvalidIdempotencyKeyLength::class);
     });
 });
