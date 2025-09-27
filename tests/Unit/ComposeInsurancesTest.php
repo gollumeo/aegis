fdac43c99ab777eca3e989aa414cdb608a723028
Golluméo <p.mauriello54@gmail.com>
2025-09-27 12:12:22 +0200

    test(ComposeInsurancesTest): add test for missing idempotency header scenario

--- tests/Unit/ComposeInsurancesTest.php
+++ tests/Unit/ComposeInsurancesTest.php
@@ -2,8 +2,23 @@
 
 declare(strict_types=1);
 
+use Gollumeo\Aegis\Domain\Exceptions\MissingIdempotencyHeader;
+use Gollumeo\Aegis\Domain\Policies\Composites\ComposeInsurances;
+use Gollumeo\Aegis\Domain\Policies\EnsureIdempotencyHeaders;
+
 describe('Unit: Compose Insurances', function (): void {
     it('fails if any policy fails', function (): void {
-        //
+        $composition = new ComposeInsurances([
+            new EnsureIdempotencyHeaders(),
+        ]);
+
+        $request = Request::create('/payments', 'POST');
+
+        expect(
+            /**
+             * @throws MissingIdempotencyHeader
+             */
+            fn () => $composition->assert($request)
+        )->toThrow(MissingIdempotencyHeader::class);
     });
 });
