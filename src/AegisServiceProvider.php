af32597743f39f2a1a3cb4d5c4d1a409dc3e6f8d
Golluméo <p.mauriello54@gmail.com>
2025-09-28 20:25:34 +0200

    feat(AegisServiceProvider): implement binding for Insurance interface to compose policies

--- src/AegisServiceProvider.php
+++ src/AegisServiceProvider.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Gollumeo\Aegis;
 
+use Gollumeo\Aegis\Application\Contracts\Insurance;
+use Gollumeo\Aegis\Domain\Policies\Composites\ComposeInsurances;
 use Gollumeo\Aegis\Infrastructure\Http\Middleware\Idempotency;
+use Gollumeo\Aegis\Support\AegisConfig;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 final class AegisServiceProvider extends ServiceProvider
@@ -16,5 +20,19 @@ final class AegisServiceProvider extends ServiceProvider
             paths: [__DIR__.'/../config/aegis.php' => config_path('aegis.php')],
             groups: 'aegis-config'
         );
+    }
+
+    public function register(): void
+    {
+        $this->app->bind(Insurance::class, function (Application $app): Insurance {
+            $policies = [];
+            foreach (AegisConfig::policies() as $class) {
+                /** @var Insurance $policy */
+                $policy = $app->make($class);
+                $policies[] = $policy;
+            }
+
+            return new ComposeInsurances($policies);
+        });
     }
 }
