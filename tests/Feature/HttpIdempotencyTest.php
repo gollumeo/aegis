3e484e0a8150d181384a34728a84e4afe0046cf4
Golluméo <p.mauriello54@gmail.com>
2025-09-30 15:01:19 +0200

    feat(HttpIdempotencyTest): add test for successful request response

--- tests/Feature/HttpIdempotencyTest.php
+++ tests/Feature/HttpIdempotencyTest.php
@@ -2,12 +2,23 @@
 
 declare(strict_types=1);
 
+use Gollumeo\Aegis\Support\AegisConfig;
 use Gollumeo\Aegis\Tests\TestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 describe('Feature: Http Idempotency', function (): void {
     it('musts throw a 428 if header is missing', function (): void {
         /** @var TestCase $this */
         $response = $this->post('/payments');
         expect($response->status())->toBe(428);
+    });
+
+    it('musts send a 201 if the request passes', function (): void {
+        /** @var TestCase $this */
+        $response = $this->post('/payments', [], [
+            AegisConfig::headerName() => $this::VALID_KEY,
+        ]);
+
+        expect($response->status())->toBe(Response::HTTP_CREATED);
     });
 });
